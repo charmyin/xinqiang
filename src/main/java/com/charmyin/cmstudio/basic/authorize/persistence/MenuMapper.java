77d8111a0b9554569391351f072976647757dc2e
charmyin <976582265@qq.com>
2013-08-06 23:06:43 +0800

    Continue developing menu module~

--- src/main/java/com/charmyin/cmstudio/basic/authorize/persistence/MenuMapper.java
+++ src/main/java/com/charmyin/cmstudio/basic/authorize/persistence/MenuMapper.java
@@ -3,12 +3,14 @@ package com.charmyin.cmstudio.basic.authorize.persistence;
 import java.util.List;
 
 import com.charmyin.cmstudio.basic.authorize.vo.Menu;
+import com.charmyin.cmstudio.basic.initial.SQLMapper;
 
 /**
  * Mybatis Mapper Interface used for menu operation
  * @author charmyin
  *
  */
+@SQLMapper
 public interface MenuMapper {
 	
 	/**
@@ -30,4 +32,30 @@ public interface MenuMapper {
 	 * @return
 	 */
 	public List<Menu> getMenuLike(Menu menu);
+	
+	/**
+	 * Get menu by menu id
+	 * @param id
+	 * @return
+	 */
+	public Menu getMenuById(String id);
+	
+	/**
+	 * Insert a piece of menu to table
+	 * @param menu
+	 * @return
+	 */
+	public Menu insertMenu(Menu menu);
+	
+	/**
+	 * Update menu by menu object which must contain id
+	 * @param menu
+	 */
+	public void updateMenu(Menu menu);
+	
+	/**
+	 * Delete menus by id string array
+	 * @param ids
+	 */
+	public void deleteMenu(String[] ids);
 }
--- src/main/java/com/charmyin/cmstudio/basic/authorize/vo/Menu.java
+++ src/main/java/com/charmyin/cmstudio/basic/authorize/vo/Menu.java
@@ -9,10 +9,61 @@ public class Menu {
 	
 	private int id;
 	private String name;
-	private int parent_id;
-	private String link_url;
-	private String order_number;
+	private int parentId;
+	private String linkUrl;
+	private String orderNumber;
 	private String remark;
-	private String full_permission;
-	private String read_permission;
+	private String fullPermission;
+	private String readPermission;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getParentId() {
+		return parentId;
+	}
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+	public String getLinkUrl() {
+		return linkUrl;
+	}
+	public void setLinkUrl(String linkUrl) {
+		this.linkUrl = linkUrl;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getFullPermission() {
+		return fullPermission;
+	}
+	public void setFullPermission(String fullPermission) {
+		this.fullPermission = fullPermission;
+	}
+	public String getReadPermission() {
+		return readPermission;
+	}
+	public void setReadPermission(String readPermission) {
+		this.readPermission = readPermission;
+	}
+	
+	
 }
